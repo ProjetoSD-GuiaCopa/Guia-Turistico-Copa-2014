45ea16038fb573545b0c2b8d4a2dd9f7a9dd69b7
Raissa Albano <raioalbano@gmail.com>
2013-08-26 00:28:42 -0300

    conexao com riak em jogos_cadastro.py

--- src/jogos_cadastro.py
+++ src/jogos_cadastro.py
@@ -1,2 +1,16 @@
+import riak
+
+
+
 def index(req):
-    return "Hello World! This is a python script!";
+	myClient = riak.RiakClient(pb_port=8087, protocol='http')
+
+	myBucket = myClient.bucket('test')
+
+	val1 = 1
+	key1 = myBucket.new('one', data=val1)
+	key1.store()
+
+	fetched1 = myBucket.get('one')
+
+	return fetched1.data
